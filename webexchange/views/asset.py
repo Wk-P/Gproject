d2c3e13572972af0a6cf32ca570973c7153b2561
Soar <weekend2000119@gmail.com>
2023-03-09 19:08:59 +0900

    wallet verification losed     filter get 'id' is integer     exclude 'id' no effect

--- webexchange/views/asset.py
+++ webexchange/views/asset.py
@@ -4,27 +4,33 @@ from ..views import *
 class asset(View):
     def get(self, request, **kwargs):
         username = kwargs.get('username')
-        return render(request, 'asset.html', {'username': username})
-
+        if username_check(username):
+            return render(request, 'asset.html', {'username': username})
+        else:
+            return redirect('404')
+        
     def post(self, request, **kwargs):
         
         # users request amount verification and send a request data package with wallet amount from users' local (or from server)
         # 从服务器端直接验证
-        username = kwargs.get('username')
-        wallet = kwargs.get('wallet')
         # wallet data from users
-            
-        wallet_amount = kwargs.get('amount')
+
+        username = kwargs.get('username')
+        
+        if 'verify' in request.POST:
         # 从服务器端数据库中找出用户服务器的数据
         # get details wallet data from database
         # take_wallet():
         #   pass 
-
-        if username_check(username):
-            self.url = 'wallet.html'
-            self.context['username'] = username
-
-
+            # verification information
+            amount = request.POST.get('amount')
+            coin_type = request.POST.get('coin_type')
+            if get_verification_information(username):
+                print("Yes")
+                return render(request, 'asset.html')
+            else:
+                return redirect('404')
+            # get user asset data from database
             '''
             # 验证函数: 用户名和钱包数据
             # verify(name, wallet):
@@ -39,10 +45,4 @@ class asset(View):
             #     'status': 'ok',
             #     'status': '
             # }
-        
 
-        else:
-            self.url = '404.html'
-            return render(request, self.url, self.context)
-        return render(request, self.url, self.context)
-        pass

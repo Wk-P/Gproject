d18ab57fd0abfba40bf9fc4c575653db0a10461e
Wk-P <weekend2000@gmail.com>
2023-03-20 19:42:44 +0900

    update

--- exchange/webexchange/static/wallet_manage/wallet_manage.js
+++ exchange/webexchange/static/wallet_manage/wallet_manage.js
@@ -9,9 +9,18 @@ window.onload = () => {
                 'X-CSRFToken': csrftoken,
             }
         }).then(response => response.json())
-        .then(data => {
-                console.log(data.assets[0].wallet_ID);
-                $("#test").text(data.assets[0].wallet_ID);
-        });
+            .then(data => {
+                console.log(data.assets);
+                assets = data.assets
+                for (var i in assets) {
+                    for (var j in assets[i]) {
+                        if (j != 'wallet_ID') {
+                            $("#test").text($("#test").text() + " | " + assets[i][j]);
+                        } else {
+                            $("#test").text(assets[i][j]);
+                        }
+                    }
+                }
+            });
     });
 }

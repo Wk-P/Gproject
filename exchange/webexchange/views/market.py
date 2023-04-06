838b4a5e2dc636c30d0eb0f883d3ff71128394c7
Soar <weekend2000119@gmail.com>
2023-04-07 05:10:27 +0900

    market dynamic data update

--- exchange/webexchange/views/market.py
+++ exchange/webexchange/views/market.py
@@ -1,34 +1,74 @@
 from webexchange.views.common.utils import *
 
-# data dict
-response_data = {}
-
 def toJson(string):
     return json.loads(string)
 
-async def fetch_data():
-    uri_prices = "wss://ws.coincap.io/prices?assets=bitcoin,ethereum,litecoin"
+def fetch_prices_data(coin_list):
+    uri_prices = "wss://ws.coincap.io/prices?assets=" + ','.join(coin_list)
+    async def fetch_data():
+        async with websockets.connect(uri_prices) as websocket_prices:
+            try:
+                prices_redata = await websocket_prices.recv()
+                prices_redata = toJson(prices_redata)
+                return prices_redata
+            except websockets.exceptions.ConnectionClosed as e:
+                print(f"Error: {e}. Closing websocket connection...")
+                await websocket_prices.close()
+                return None
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        prices_data = loop.run_until_complete(fetch_data())
+        return prices_data
+    finally:
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
+
+def fetch_trades_data():
     uri_trades = "wss://ws.coincap.io/trades/binance"
-    async with websockets.connect(uri_prices) as websocket_prices:
-        prices_redata = await websocket_prices.recv()
-        response_data['prices-data'] = prices_redata
+    async def fetch_data():
+        async with websockets.connect(uri_trades) as websocket_trades:
+            try:
+                trades_redata = await websocket_trades.recv()
+                trades_redata = toJson(trades_redata)
+                return trades_redata
+            except websockets.exceptions.ConnectionClosed as e:
+                print(f"Error: {e}. Closing websocket connection...")
+                await websocket_trades.close()
+                return None
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        trades_data = loop.run_until_complete(fetch_data())
+        return trades_data
+    finally:
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
 
-    async with websockets.connect(uri_trades) as websocket_trades:
-        trades_redata = await websocket_trades.recv()
-        trades_redata = toJson(trades_redata)
-        response_data['trades-data'] = trades_redata
+def fetch_market_information():
+    coin_list = []
+    url = 'https://api.coincap.io/v2/assets'
+    coin_data = toJson(requests.request("GET", url).content)['data']
+    for i in range(20):
+        coin_list.append(coin_data[i]['id'])
 
-# market
+    prices_data = fetch_prices_data(coin_list)
+    trades_data = fetch_trades_data()
+
+    response_data = {}
+    response_data['assets_data'] = coin_data
+    response_data['prices-data'] = prices_data
+    response_data['trades-data'] = trades_data
+
+    return response_data
+
 class market(View):
-    async def get(self, request):
+    def get(self, request):
         return render(request, 'market.html')
-    
-    async def post(self, request):
-        loop = asyncio.get_event_loop()
-        if loop.is_running():
-            await fetch_data()
-        else:
-            await asyncio.run(fetch_data())
-        
-        print(response_data)
-        return JsonResponse(response_data)
+
+    def post(self, request):
+        try:
+            response_data = fetch_market_information()
+            return JsonResponse(response_data)
+        except Exception as e:
+            return JsonResponse({'error': str(e)})

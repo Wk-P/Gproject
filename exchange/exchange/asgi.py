643cd6b8be49c2cc02a6e0de21004c20455c10cc
Soar <weekend2000119@gmail.com>
2023-04-08 19:41:39 +0900

    uwsgi init update

--- exchange/exchange/asgi.py
+++ exchange/exchange/asgi.py
@@ -1,16 +1,6 @@
 import os
-import sys
-from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-from django.conf import settings
-import uwsgi
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Gproject.settings')
 
-application = StaticFilesHandler(get_wsgi_application())
-
-uwsgi.set_python_optimize()
-uwsgi.add_file_monitor(
-    pth=os.path.abspath(__file__),
-    callback=lambda *args, **kwargs: sys.exit(0)
-)
+application = get_asgi_application()

12ee51ddfc970591c3f8eb2972d3623e21aa5749
Soar <weekend2000119@gmail.com>
2023-04-08 21:27:36 +0900

    static path update

--- exchange/exchange/settings.py
+++ exchange/exchange/settings.py
@@ -122,7 +122,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = BASE_DIR / 'webexchange/static'
 STATICFILES_DIRS = [
     BASE_DIR / "webexchange/static",
 ]

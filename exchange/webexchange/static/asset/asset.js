d18e3ce5bc589e6684223a9fb9378908888125e7
Wk-P <weekend2000@gmail.com>
2023-04-26 16:52:29 +0900

    未完成的asset界面处理

--- exchange/webexchange/static/asset/asset.js
+++ exchange/webexchange/static/asset/asset.js
@@ -4,15 +4,17 @@ function generate_table_contents(table_id, tr_size, td_id_arr, td_contents) {
         let tr = document.createElement('tr');
         for (td_id in td_id_arr) {
             td = document.createElement('td');
-            td.id = td_id;
+            td.id = td_id_arr[td_id];
+            td.innerHTML = td_contents[td_id];
             tr.appendChild(td);
         }
         table.appendChild(tr);
     }
 }
 
-
 window.onload = () => {
+    let btn_count = 1;
+
     const username = $("#user-name").html();
     const csrftoken = getCookie('csrftoken');
     const td_id_array = [
@@ -37,6 +39,8 @@ window.onload = () => {
     data.click = 'no';
     params.body = JSON.stringify(data);
 
+    table_id = "asset_list"
+
     fetch(`/asset/${username}/`, params)
         .then(response => response.json())
         .then(data => {
@@ -44,14 +48,34 @@ window.onload = () => {
             console.log(data.asset_data);
             if (data.alert == 'success') {
                 // asset data is completed
+
             } else if (data.alert == 'incomplete') {
                 // asset data is None
-                
+                let table = document.getElementById(table_id);
+                for (wallet_id of data.wallets_id) {
+                    let tr = document.createElement('tr');
+                    for (index in td_id_array) {
+                        let td = document.createElement('td');
+                        td.id = td_id_array[index]
+                        if (td.id == `wallet_ID-${username}`) td.innerHTML = wallet_id;
+                        if (td.id == `veriy_btn-${username}`) {
+                            let btn = document.createElement('button');
+                            btn.id = td.id + btn_count.toString();
+                            btn.class = "verify_btn";
+                            btn_count = btn_count + 1;
+                            btn.innerHTML = 'Verify';
+                            td.appendChild(btn);
+                        }
+                        else td.innerHTML = 'none';
+                        tr.appendChild(td);
+                    }
+                    table.appendChild(tr);
+                }
             }
             else {
                 alert(data.alert);
             }
-            // td = document.createElement('td')
+            td = document.createElement('td')
         })
         .catch(error => {console.log(error)});
 }

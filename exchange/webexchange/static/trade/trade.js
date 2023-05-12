7c84a0e07943ccefb57fd68c0eab01489b952e0b
Soar <weekend2000119@gmail.com>
2023-05-12 20:23:21 +0900

    local update

--- exchange/webexchange/static/trade/trade.js
+++ exchange/webexchange/static/trade/trade.js
@@ -1,3 +1,177 @@
+// function timetrans(date) {
+//     var date = new Date(date);//如果date为13位不需要乘1000
+//     var Y = date.getFullYear() + '-';
+//     var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+//     var D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate()) + ' ';
+//     var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+//     var m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+//     var s = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds());
+//     return Y + M + D + h + m + s;
+// }
+
+// window.onload = () => {
+
+
+//     // 定义画布宽度、高度和边距
+//     const width = $("#chart").width() * 0.9;
+//     const height = $("#chart").height() * 0.9;
+//     const margin = { top: 10, right: 30, bottom: 30, left: 60 };
+
+
+//     // 创建SVG元素
+//     const svg = d3.select('#chart')
+//         .append('svg')
+//         .attr('width', width,)
+//         .attr('height', height,);
+
+
+
+
+//     // 获取当前交易对的价格
+//     // const symbol = 'BTC_USDT';
+//     // fetch(`https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`, { mode: 'cors' })
+//     //     .then(response => response.json())
+//     //     .then(data => console.log(`当前 ${symbol} 价格为 ${data.price}`))
+//     //     .catch(error => console.error(error));
+
+
+
+//     // 订阅 WebSocket 实时推送的 K 线数据
+//     const ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@kline_1s');
+
+
+
+
+//     ws.onmessage = event => {
+//         const data = JSON.parse(event.data);
+//         console.log('hightest price: ', data.k.h);
+//         console.log('lowest price: ', data.k.l);
+//         console.log('open price: ', data.k.o);
+//         console.log('close price: ', data.k.c);
+//         console.log('time:', timetrans(data.k.T));
+//         const trade_data = [
+//             { time: timetrans(data.k.T), open: data.k.o, high: data.k.h, low: data.k.l, close: data.k.c },
+//             { time: timetrans(data.k.T), open: data.k.o, high: data.k.h, low: data.k.l, close: data.k.c },
+//             // { time: '2021-01-03', open: 10500, high: 11000, low: 10000, close: 10200 },
+
+
+//         ];
+
+//         // 定义K线图的比例尺和坐标轴
+//         const xScale = d3.scaleBand().range([margin.left, width - margin.right]);
+//         const yScale = d3.scaleLinear().range([height - margin.bottom, margin.top]);
+//         const xAxis = d3.axisBottom(xScale);
+//         const yAxis = d3.axisLeft(yScale);
+
+//         // 更新K线图
+//         const updateChart = (trade_data) => {
+//             // 计算比例尺的域
+//             const minTime = d3.min(trade_data, d => d.time);
+//             const maxTime = d3.max(trade_data, d => d.time);
+//             const minValue = d3.min(trade_data, d => d.low);
+//             const maxValue = d3.max(trade_data, d => d.high);
+
+
+//             // 更新比例尺的域
+//             xScale.domain(trade_data.map(d => new Date(d.time)));
+//             yScale.domain([minValue, maxValue]);
+
+//             // 添加坐标轴
+//             svg.select('.x-axis')
+//                 .attr('transform', `translate(0, ${height - margin.bottom})`)
+//                 .call(xAxis);
+
+
+
+//             svg.select('.y-axis')
+//                 .attr('transform', `translate(${margin.left}, 0)`)
+//                 .call(yAxis);
+
+//             // 更新K线图的数据
+//             const candles = svg.selectAll('.candle').data(trade_data);
+
+//             // 移除已经不存在的数据
+//             candles.exit().remove();
+
+//             // 添加新的K线图数据
+//             const newCandles = candles.enter()
+//                 .append('g')
+//                 .attr('class', 'candle')
+//                 .attr('transform', d => `translate(${xScale(new Date(d.time)) - xScale.bandwidth() / 20}, 0)`);
+
+
+//             var color = 'green';
+//             newCandles.append('line')
+//                 .attr('class', 'high-low')
+//                 .attr('x1', xScale.bandwidth() / 5)
+//                 .attr('x2', xScale.bandwidth() / 5)
+//                 .attr('y1', d => yScale(d.high))
+//                 .attr('y2', d => yScale(d.low))
+//                 .attr('stroke', d => d.open < d.close ? 'green' : 'red');
+
+//             var color_two = 'red';
+
+//             newCandles.append('rect')
+//                 .attr('class', 'open-close')
+//                 .attr('x', 0)
+//                 .attr('y', d => yScale(Math.max(d.open, d.close)))
+//                 .attr('width', xScale.bandwidth() / 5)   //柱宽
+//                 .attr('fill', d => d.open < d.close ? 'green' : 'red')
+//                 .attr('height', d => {
+//                     if (d.open === d.close) {
+//                         return 1;
+//                     } else {
+//                         return yScale(Math.min(d.open, d.close)) - yScale(Math.max(d.open, d.close));
+//                     }
+//                 });
+
+//                 // 更新K线图的位置和大小
+//                 candles.merge(newCandles)
+//                 .transition()
+//                 .duration(1000)
+//                 .attr('transform', d => `translate(${xScale(new Date(d.time)) - xScale.bandwidth() / 20}, 0)`);
+
+//               candles.merge(newCandles)
+//                 .select('.high-low')
+//                 .transition()
+//                 .duration(1000)
+//                 .attr('y1', d => yScale(d.high))
+//                 .attr('y2', d => yScale(d.low))
+//                 .attr('stroke', d => d.open < d.close ? color : color_two);
+
+//               candles.merge(newCandles)
+//                 .select('.open-close')
+//                 .transition()
+//                 .duration(1000)
+//                 .attr('y', d => yScale(Math.max(d.open, d.close)))
+//                 .attr('height', d => d.open === d.close ? 1 : Math.abs(yScale(d.open) - yScale(d.close)))
+//                 .attr('fill', d => d.open < d.close ? color : color_two);
+
+
+//             }
+//         updateChart(trade_data)
+//         // console.log(updateChart);
+
+//         // 选中DOM元素并添加事件监听
+//         $('#time-buttons button').on('click', function () {
+//             // 处理事件
+//         });
+
+
+
+
+
+//         // 连接服务器
+//         // const socket = io.connect('http://localhost');
+
+//         // 监听事件
+//         // socket.on('update', function (data) {
+//         // 更新数据
+//         // });
+//     }
+// }    
+
+
 const margin = { top: 10, right: 30, bottom: 30, left: 60 };
 
 // 绘制标题
@@ -35,14 +209,18 @@ function drawAxisX(svg, width, height, margin) {
         .range([0, width - margin.left - margin.right])
 
     var axis = d3.axisBottom(scale)
-    // .ticks(10) //x轴上面取值范围分为几分
+        .ticks(10) //x轴上面取值范围分为几分
     // .tickFormat(v => {
     // return dates[v]
     // })
 
     var g = svg.append('g')
         .attr('transform', 'translate(' + margin.left + ',' + (height - margin.bottom) + ')')
-    // .call(axis)
+        .call(axis)
+
+
+
+
     return g
 }
 
@@ -97,7 +275,7 @@ function drawCandlestick(svg, width, height, trade_data, gx, yScale, margin) {
     console.log(dates, parseInt(dates[0]))
 
     let datesObjects = []
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0;i < 10;i++) {
         if (i < length) {
             datesObjects[i] = dates[i]
         }
@@ -105,7 +283,7 @@ function drawCandlestick(svg, width, height, trade_data, gx, yScale, margin) {
             datesObjects[i] = dates[0] + i;
         }
     }
-
+    
     const xScale = d3.scaleLinear()
         .domain([parseInt(datesObjects[0]), parseInt(datesObjects[datesObjects.length - 1])])
         .range([0, width - margin.left - margin.right])
@@ -362,7 +540,7 @@ window.onload = () => {
         }
 
         params.body = JSON.stringify(data)
-        fetch(`/trade/${username}/`, params)
+        fetch(`/trade/${username}`, params)
             .then(response => response.json())
             .then(data => {
                 console.log("DATA:", data);;
@@ -370,7 +548,6 @@ window.onload = () => {
             })
             .catch(error => console.log(error))
     });
-
 
     const max_size = 10;
     var g_arr = new Array();
@@ -393,47 +570,50 @@ window.onload = () => {
     var text = svg.append('text');
 
 
-    params.body = JSON.stringify(data);
-    setInterval(() => {
-        fetch(`/trade/${username}`, params)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.price_data);        
-            })
-        // console.log(trade_data.length)
-        // text.selectAll('*').remove();
-        // text.attr('x', width - margin.right)
-        //     .attr('y', margin.top / 2)
-        //     .attr('font-size', '0.85em')
-        //     .attr('fill', '#666')
-        //     .attr('text-anchor', 'end')
-        //     .attr('dominant-baseline', 'hanging')
 
-        // // 使用setTimeout函数延迟数据处理
-        // if (trade_data.length < max_size) {
-        //     console.log(timetrans(data.k.T))
-        //     trade_data.push([timetrans(data.k.T), parseFloat(data.k.o), parseFloat(data.k.h), parseFloat(data.k.l), parseFloat(data.k.c)])
-        //     // trade_data.push([timedata.k.T, parseFloat(data.k.o), parseFloat(data.k.h), parseFloat(data.k.l), parseFloat(data.k.c)])
-        // }
-
-        // text.text(formatText(trade_data[trade_data.length - 1]))
-        // // 计算蜡烛图实线宽度
-        // const getCandlestickWidth = dataLength => (width - margin.left - margin.right) / dataLength - 3
-
-        // drawCandlestick(svg, width, height, trade_data, gx, yScale, margin)
-
-        // drawFocusLayout(svg, trade_data, width, height, xScale, yScale, margin, text, formatText)
-        // drawTitle(data.name)
-    }, 1000) // 延迟500毫秒进行数据处理
     // 订阅 WebSocket 实时推送的 K 线数据
 
-    // const ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@kline_1s');
+    const ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@kline_1s');
 
-    // ws.onmessage = event => {
-    // const data = JSON.parse(event.data);
-    // console.log('hightest price: ', data.k.h);
-    // console.log('lowest price: ', data.k.l);
-    // console.log('open price: ', data.k.o);
-    // console.log('close price: ', data.k.c);
-    // console.log('time:', timetrans(data.k.T));
+    ws.onmessage = event => {
+        const data = JSON.parse(event.data);
+        // console.log('hightest price: ', data.k.h);
+        // console.log('lowest price: ', data.k.l);
+        // console.log('open price: ', data.k.o);
+        // console.log('close price: ', data.k.c);
+        // console.log('time:', timetrans(data.k.T));
+        console.log(trade_data.length)
+
+
+        text.selectAll('*').remove();
+        text.attr('x', width - margin.right)
+            .attr('y', margin.top / 2)
+            .attr('font-size', '0.85em')
+            .attr('fill', '#666')
+            .attr('text-anchor', 'end')
+            .attr('dominant-baseline', 'hanging')
+
+        // 使用setTimeout函数延迟数据处理
+        setTimeout(() => {
+            if (trade_data.length < max_size) {
+                console.log(timetrans(data.k.T))
+                trade_data.push([timetrans(data.k.T), parseFloat(data.k.o), parseFloat(data.k.h), parseFloat(data.k.l), parseFloat(data.k.c)])
+                // trade_data.push([timedata.k.T, parseFloat(data.k.o), parseFloat(data.k.h), parseFloat(data.k.l), parseFloat(data.k.c)])
+            }
+
+            text.text(formatText(trade_data[trade_data.length - 1]))
+            // 计算蜡烛图实线宽度
+            const getCandlestickWidth = dataLength => (width - margin.left - margin.right) / dataLength - 3
+
+            drawCandlestick(svg, width, height, trade_data, gx, yScale, margin)
+
+
+
+
+            drawFocusLayout(svg, trade_data, width, height, xScale, yScale, margin, text, formatText)
+            // drawTitle(data.name)
+        }, 1000) // 延迟500毫秒进行数据处理
+    }
+
+
 }
--- exchange/webexchange/views/trade.py
+++ exchange/webexchange/views/trade.py
@@ -1,9 +1,6 @@
 from webexchange.views.common.utils import *
 
-response_data = []
-
 async def price_fetch():
-    global response_data
     async with websockets.connect('wss://stream.binance.com:9443/ws/btcusdt@kline_1s') as ws:
         while True:
             # 接收响应数据
@@ -11,37 +8,33 @@ async def price_fetch():
             # 判断数据是否为完整的 JSON 字符串
             try:
                 response_data.append(json.loads(response))
-                print(response_data)
             except json.JSONDecodeError:
                 # 如果不是完整的 JSON 字符串，继续接收数据并拼接
                 continue
             # 延时 1 秒，避免 CPU 过高
-            await asyncio.sleep(0.2)
+            await asyncio.sleep(1)
 
+def asyncloop():
+    asyncio.get_event_loop().run_until_complete(price_fetch())
 
-async def get_price():
-    await asyncio.gather(price_fetch(), trade.post())
-
-async def asyncloop():
-    await asyncio.run(get_price())
-
-# price matching
+# price matching    
 order_manager = OrderManager()
 consumer = OrderConsumer(order_manager=order_manager)
 consumer.start()
 
 # websocket -> price fetching
-asyncloop()
+response_data = []
+socket = threading.Thread(target=asyncloop, args=(response_data))
+socket.start()
+
+
 # views request handle
-
-
 class trade(View):
     async def get(self, request, **kwargs):
         username = kwargs.get('username')
         return render(request, 'trade.html', context={'username': username})
 
     async def post(self, request, **kwargs):
-        global response_data
         response = {'alert': None}
         data = json.loads(request.body.decode('utf-8'))
         if 'trade' in data['reqtype']:
@@ -51,16 +44,9 @@ class trade(View):
             producer.join()
             response['alert'] = f"{data['type']} 提交成功!"
 
-
-        # 返回异步成交数据
-        if len(order_manager.ordered_orders) > 0:
-            response['orderd'] = order_manager.ordered_orders
-
-        if len(response_data) > 0:
-            response['price_data'] = response_data[-1]
-        else:
-            response['price_data'] = {}
-
-        response['alert'] = "POST"
+        if 'match' in data['reqtype']:
+            # 返回异步成交数据
+            if len(order_manager.ordered_orders) > 0:
+                response['orderd'] = order_manager.ordered_orders
 
         return JsonResponse(response)

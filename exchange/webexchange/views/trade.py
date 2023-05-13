b0ba7d4dfa0006d6bd86ca5ca0081480c101bac7
Soar <weekend2000119@gmail.com>
2023-05-13 12:30:52 +0900

    UPDATE trade.py

--- exchange/webexchange/views/trade.py
+++ exchange/webexchange/views/trade.py
@@ -1,32 +1,9 @@
 from webexchange.views.common.utils import *
 
-async def price_fetch():
-    async with websockets.connect('wss://stream.binance.com:9443/ws/btcusdt@kline_1s') as ws:
-        while True:
-            # 接收响应数据
-            response = await ws.recv()
-            # 判断数据是否为完整的 JSON 字符串
-            try:
-                response_data.append(json.loads(response))
-            except json.JSONDecodeError:
-                # 如果不是完整的 JSON 字符串，继续接收数据并拼接
-                continue
-            # 延时 1 秒，避免 CPU 过高
-            await asyncio.sleep(1)
-
-def asyncloop():
-    asyncio.get_event_loop().run_until_complete(price_fetch())
-
 # price matching    
 order_manager = OrderManager()
 consumer = OrderConsumer(order_manager=order_manager)
 consumer.start()
-
-# websocket -> price fetching
-response_data = []
-socket = threading.Thread(target=asyncloop, args=(response_data))
-socket.start()
-
 
 # views request handle
 class trade(View):
@@ -37,12 +14,17 @@ class trade(View):
     async def post(self, request, **kwargs):
         response = {'alert': None}
         data = json.loads(request.body.decode('utf-8'))
+
         if 'trade' in data['reqtype']:
             producer = OrderProducer(
-                order_manager=order_manager, order_type=data['type'], stock_name=data['stockname'], price=data['price'], quantity=data['quantity'])
+                order_manager=order_manager, order_type=data['order']['type'], 
+                stock_name=data['order']['stockname'], 
+                price=data['order']['price'], 
+                quantity=data['order']['quantity']
+            )
             producer.start()
             producer.join()
-            response['alert'] = f"{data['type']} 提交成功!"
+            response['alert'] = f"{data['order']['type']} 提交成功!"
 
         if 'match' in data['reqtype']:
             # 返回异步成交数据

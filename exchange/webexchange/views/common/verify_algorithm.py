fee13f871aa37a2e38dcafbae0e780f3242835cb
Soar <weekend2000119@gmail.com>
2023-05-14 20:41:41 +0900

    UPDATE verify_alg

--- exchange/webexchange/views/common/verify_algorithm.py
+++ exchange/webexchange/views/common/verify_algorithm.py
@@ -4,14 +4,14 @@ from .zk_snarks import generate_proof, verify_proof
 from .utils import *
 
 def combine_data(user_data, all_user_data):
+    data = []
     with open('output_data.json', 'a', encoding='utf-8') as f:
-        data = []
         try:
             # 准备输入数据
             for i in range(len(all_user_data)):
                 asset_string = ""
                 for index in range(len(all_user_data[i]['assets'])):
-                    asset_string =+ all_user_data[i]['assets'][index][0]['type'] + str(all_user_data[i]['assets'][index][0]['asset_amount'])
+                    asset_string += all_user_data[i]['assets'][index][0]['asset_type'] + str(all_user_data[i]['assets'][index][0]['asset_amount'])
                 
                 data.append(bytes(all_user_data[i]['user_ID'] + asset_string, encoding='utf8'))
 
@@ -22,24 +22,19 @@ def combine_data(user_data, all_user_data):
                     'public': 'my_public_key'
                 }
             }
-
             # 调用 Merkle Tree 函数
-            try:
-                tree = MerkleTree(data)
-                merkle_root_hash = tree.get_root_hash().hex()
-                print(tree)
-            except Exception as e:
-                print("MERKLE ERROR", e)
+            tree = MerkleTree(data)
+            merkle_root_hash = tree.get_root_hash().hex()
 
             # 调用 ZK-SNARKs 函数
             proof, signal = generate_proof(input_data['zk_data']['secret'], input_data['zk_data']['public'])
             verification_result = verify_proof(proof, signal, input_data['zk_data']['public'])
-            try:
-                print(proof)
-                print(signal)
-                print(verification_result)
-            except Exception as e:
-                print("ERROR",e)
+            # try:
+            #     print(proof)
+            #     print(signal)
+            #     print(verification_result)
+            # except Exception as e:
+            #     print("ERROR",e)
 
             # 存储输出数据
             output_data = {
@@ -61,3 +56,4 @@ def combine_data(user_data, all_user_data):
             json.dump(output_data, f)
         except Exception as e:
             print(e)
+            
